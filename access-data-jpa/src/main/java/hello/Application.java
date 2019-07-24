08ef83dc12c545b43f83066e6b1caf1cd9a01694
zajan <aniaaa94@gmail.com>
2019-07-24 13:05:22 +0200

    access-data-jpa / implement storing and fetching data

---
+++ access-data-jpa/src/main/java/hello/Application.java
@@ -0,0 +1,65 @@
+package hello;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+
+    // CommandLineRunner bean executes logic after the application context is loaded and started
+    // Thanks to Java 8 we can implement it with lambda expression because it is a single method interface
+    @Bean
+    public CommandLineRunner demo(CustomerRepository repository) {
+        return (args) -> {
+            repository.save(new Customer("Jack", "Bauer"));
+            repository.save(new Customer("Chloe", "O'Brian"));
+            repository.save(new Customer("Kim", "Bauer"));
+            repository.save(new Customer("David", "Palmer"));
+            repository.save(new Customer("Michelle", "Dessler"));
+
+            // fetch all customers
+            log.info("Customers found with findAll():");
+            log.info("-------------------------------");
+            for (Customer customer : repository.findAll()) {
+                log.info(customer.toString());
+            }
+            log.info("");
+
+            // fetch an individual customer by ID
+            repository.findById(1L)
+                    .ifPresent(customer -> {
+                        log.info("Customer found with findById(1L):");
+                        log.info("--------------------------------");
+                        log.info(customer.toString());
+                        log.info("");
+                    });
+
+            // fetch customers by first name
+            log.info("Customer found with findByFirstName('Kim'):");
+            log.info("--------------------------------------------");
+            repository.findByFirstName("Kim").forEach(kim -> {
+                log.info(kim.toString());
+            });
+
+            // fetch customers by last name
+            log.info("Customer found with findByLastName('Bauer'):");
+            log.info("--------------------------------------------");
+            repository.findByLastName("Bauer").forEach(bauer -> {
+                log.info(bauer.toString());
+            });
+
+            log.info("");
+
+        };
+    }
+}
